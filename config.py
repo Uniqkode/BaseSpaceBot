a09215d6def78dab3a0fc9c4b7b3ff079acba42c
uniqkode <yungboox@gmail.com>
2024-10-12 12:00:53 +0100

    #2

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
 load_dotenv()
 
+# Get environment variables and cast to correct types
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
+API_ID = int(os.getenv("API_ID")) if os.getenv("API_ID") else None
 API_HASH = os.getenv("API_HASH")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL")) if os.getenv("LOG_CHANNEL") else None
 DB_NAME = os.getenv("DB_NAME")
-AUTH_USERS = set(int(x) for x in os.getenv("AUTH_USERS").split())
+AUTH_USERS = set(int(x) for x in os.getenv("AUTH_USERS", "").split()) if os.getenv("AUTH_USERS") else set()
 DB_URL = os.getenv("DB_URL")
 BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True").lower() == 'true'
